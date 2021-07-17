28cdd9222ca11b9ea708edee6c04c65af2c90716
tan004 <qq563169939@yahoo.com>
2021-07-17 13:50:27 -0700

    added some style on the home page and set up the navLink for each business

--- frontend/src/components/HomePage/index.js
+++ frontend/src/components/HomePage/index.js
@@ -1,10 +1,12 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { NavLink } from 'react-router-dom';
 import { getBusiness } from '../../store/business';
+import './homepage.css'
 const Homepage = () => {
 
     const allBusiness = useSelector(state => state.business)
-    console.log(allBusiness.business)
+    // console.log(allBusiness.business)
     const dispatch = useDispatch();
 
     useEffect(() => {
@@ -12,20 +14,25 @@ const Homepage = () => {
     }, [dispatch])
 
     return (
-        <div>
-            {allBusiness.business.map(business =>
+        <div  >
+            <div className='allbusiness'>
+                {allBusiness.business.map(business =>
                 (
-                    <div>
-                        <img style={{width:300}} src={business.imgUrl} alt={business.id}/>
-                        <li key={business.id}>{business.title}</li>
-                        <li key={business.id}>{business.description}</li>
+                    <div className='business-list'>
+                        <NavLink key={business.title} to={`/business/${business.id}`}>
+
+                            <img src={business.imgUrl} alt={business.id} />
+                            <h2 key={business.id}>{business.title}</h2>
+
+                        </NavLink>
+                        <p key={business.id}>{business.description}</p>
                         <li key={business.id}>{business.address}</li>
                         <li key={business.id}>{business.city}</li>
                         <li key={business.id}>{business.state}</li>
                         <li key={business.id}>{business.zipCode}</li>
                     </div>
-                )
-            )}
+                ))}
+            </div>
         </div>
     )
 }
