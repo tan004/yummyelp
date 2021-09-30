0d2d585b71a912a45806f76d6fe324ae95322603
tan004 <ztan4@mail.sfsu.edu>
2021-09-29 18:56:48 -0700

    add css

--- frontend/src/components/FooterPage/index.js
+++ frontend/src/components/FooterPage/index.js
@@ -4,13 +4,13 @@ const FooterPage = () => {
     return (
         <div className='footer__container'>
             <div>
-            <a className="about-me" target='_blank' href='https://github.com/tan004'><i className="fab fa-github"></i></a>
+            <a className="about-me" target='_blank' href='https://github.com/tan004'><i className="fab footer-icon fa-github"></i></a>
             </div>
             <div>
-            <a className="about-me" target='_blank' href='https://www.linkedin.com/in/zhuoxin-tan-491587172/'><i className="fab fa-linkedin"></i></a>
+            <a className="about-me" target='_blank' href='https://www.linkedin.com/in/zhuoxin-tan-491587172/'><i className="fab footer-icon fa-linkedin"></i></a>
             </div>
             <div>
-            <a className="about-me" target='_blank' href='https://angel.co/u/zhuoxin-tan-1'><i className="fab fa-angellist"></i></a>
+            <a className="about-me" target='_blank' href='https://angel.co/u/zhuoxin-tan-1'><i className="fab footer-icon fa-angellist"></i></a>
             </div>
         </div>
     )
